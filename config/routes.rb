e668bb7ff0af1aa3366eb30c43b8623218b93964
locckhl <locckhl123@example.com>
2020-11-28 16:44:08 +0000

    Fix images bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
