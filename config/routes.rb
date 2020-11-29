d6db93db61ad3b3170ef7d7810d867d153322a9a
Ubuntu <ubuntu@ip-172-31-1-125.ec2.internal>
2020-11-29 14:27:27 +0000

    add User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
