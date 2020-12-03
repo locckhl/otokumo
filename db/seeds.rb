fbf11203ab29a98ecb6c8639940189e0ea6e164d
locckhl <locckhl123@gmail.com>
2020-12-03 05:02:41 +0000

    Add seeding and delete template folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create!(
+    email: "admin@admin.com",
+    name: "admin",
+    age: 21,
+    password: "admin1",
+    password_confirmation: "admin1",
+    isAdmin: true
+)
+
+
+song = Song.create!(
+    title: "Am Tham Ben Em2",
+    artist: "Son Tung",
+    user_id: admin.id
+    # source: File.read()
+ )
+ 
+song.source.attach(
+    io: File.open(Rails.root.join('public','mp3', 'Am Tham Ben Em - Son Tung M- TP [320kbps_MP3].mp3')),
+    filename: 'Am Tham Ben Em - Son Tung M- TP [320kbps_MP3].mp3',
+    content_type: 'audio/mpeg'
+)
+  
+song.image.attach(
+    io: File.open(Rails.root.join('public', 'avatar','ATBE.jpg')),
+    filename: 'ATBE.jpg'
+)
